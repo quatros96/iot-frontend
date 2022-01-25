add61f9cfe0c37a8831eb7e4e69ca4eabf31c50b
quatros96 <quatros96@gmail.com>
2022-01-25 22:22:02 +0100

    Security updates

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
-  firebase: {
-    projectId: 'iot-mgr',
-    appId: '1:350061360404:web:960a7766c363b556d7be01',
-    storageBucket: 'iot-mgr.appspot.com',
-    locationId: 'europe-west',
-    apiKey: 'AIzaSyD6UbImStQfrpE5ItiKeoaU84NslJDck4A',
-    authDomain: 'iot-mgr.firebaseapp.com',
-    messagingSenderId: '350061360404',
-  },
-  production: true
-};
+    firebase: {
+        projectId: 'iot-mgr',
+        appId: '1:350061360404:web:960a7766c363b556d7be01',
+        storageBucket: 'iot-mgr.appspot.com',
+        locationId: 'europe-west',
+        apiKey: 'AIzaSyAtPuPSvD4BUT9aAs6_OoIdQrDggVegSfs',
+        authDomain: 'iot-mgr.firebaseapp.com',
+        messagingSenderId: '350061360404',
+    },
+    production: true,
+}
